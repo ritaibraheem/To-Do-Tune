d714f145f4ca4a45f41d3d41a3af71c01da0fa58
ritaibraheem <rita.ib996@gmail.com>
2024-04-02 15:37:11 +0300

    view tasks and add task

--- app.py
+++ app.py
@@ -1,16 +1,87 @@
+from datetime import datetime
+# from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+# from htbuilder.units import percent, px
+# from htbuilder.funcs import rgba, rgb
 import streamlit as st
 import pandas as pd 
 from db_funcs import *
 from PIL import Image
 import plotly.express as px 
 
+# def image(src_as_string, **style):
+#     return img(src=src_as_string, style=styles(**style))
+
+
+# def link(link, text, **style):
+#     return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+# def layout(*args):
+
+#     style = """
+#     <style>
+#       # MainMenu {visibility: hidden;}
+#       footer {visibility: hidden;}
+#      .stApp { bottom: 105px; }
+#     </style>
+#     """
+
+#     style_div = styles(
+#         position="fixed",
+#         left=0,
+#         bottom=0,
+#         margin=px(0, 0, 0, 0),
+#         width=percent(100),
+#         color="black",
+#         text_align="center",
+#         height="auto",
+#         opacity=1
+#     )
+
+#     style_hr = styles(
+#         display="block",
+#         margin=px(8, 8, "auto", "auto"),
+#         border_style="inset",
+#         border_width=px(2)
+#     )
+
+#     body = p()
+#     foot = div(
+#         style=style_div
+#     )(
+#         hr(
+#             style=style_hr
+#         ),
+#         body
+#     )
+
+#     st.markdown(style, unsafe_allow_html=True)
+
+#     for arg in args:
+#         if isinstance(arg, str):
+#             body(arg)
+
+#         elif isinstance(arg, HtmlElement):
+#             body(arg)
+
+#     st.markdown(str(foot), unsafe_allow_html=True)
+
+
+# def footer():
+#     myargs = ["Made with ❤️ by Bella & Rita"]
+#     layout(*myargs)
+
+# *******************************************
+# App Components
+# *******************************************
+
 def color_df(val):
 	if val == "Done":
 		color = "green"
-	elif val == "Doing":
+	elif val == "In-Progress":
 		color = "orange"
 	else:
-		color = "red"
+		color = "red"  
 
 	return f'background-color: {color}'
 
@@ -18,38 +89,73 @@ st.set_page_config(
     page_title="ToDo Tune",
     page_icon="📝",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="expanded"
 )
 
-top_image = Image.open('static/banner_top.png')
+# top_image = Image.open('static/banner_top.png')
 bottom_image = Image.open('static/banner_bottom.png')
-main_image = Image.open('static/main_banner.png')
+# main_image = Image.open('static/main_banner.png')
 
-st.image(main_image,use_column_width='always')
+# st.image(main_image,use_column_width='always')
 st.title("📄 ToDo Tune")
 
-st.sidebar.image(top_image,use_column_width='auto')
-choice = st.sidebar.selectbox("Menu", ["Create Task ✅","Update Task 👨‍💻","Delete Task ❌", "View Tasks' Status 👨‍💻"])
+# st.sidebar.image(top_image,use_column_width='auto')
+choice = st.sidebar.selectbox("Menu", ["My Day 🎯","Create Task ✅","Update Task 🖊️","Delete Task ❌", "View Tasks' Status 👨‍💻"])
 st.sidebar.image(bottom_image,use_column_width='auto')
 create_table()
 
+if choice == "My Day 🎯":
+	st.subheader("My Day 🎯")
+
+	result = get_today_tasks(datetime.now())
+	# st.write(result)
+	result_df = pd.DataFrame(result,columns=['guid', 'title', 'tag', 'deadline', 'about', 'task_status', 'time_estimation', 'start_time', 'end_time', 'deadline_met'])
+	clean_df= result_df[['title', 'task_status', 'deadline']]
+	# clean_df['hours left'] = 
+
+
+	clean_df['deadline'] = pd.to_datetime(clean_df['deadline'])
+	clean_df['deadline']= clean_df['deadline'].dt.date
+
+	task_df = clean_df['task_status'].value_counts().to_frame()
+	task_df = task_df.reset_index()
+
 if choice == "Create Task ✅":
-	st.subheader("Add Item")
+	st.subheader("Add New Task")
 	col1,col2 = st.columns(2)
 
 	with col1:
-		task = st.text_area("Task To Do")
+		# task = st.text_area("Task To Do")
+		title = st.text_input("Title")
+		about = st.text_area("Description")
+		
 
 	with col2:
-		task_status = st.selectbox("Status",["ToDo","Doing","Done"])
-		task_due_date = st.date_input("Due Date")
+		tag = st.selectbox("Category",["home", "bureaucracy", "studies", "work", "exercise", "fun", "health"])
+		deadline_date = st.date_input("Due Date")
+		deadline = datetime.combine(deadline_date, datetime.max.time())
+		time_estimation = st.number_input(label="Estimated Time (hours)", min_value=0,)
+		task_status = st.selectbox("Status",["To-Do","In-Progress"])
+
+	if task_status == "In-Progress":
+			start_time=datetime.now()
+	else: start_time=None
+
+	if task_status == "Done":
+			end_time=datetime.now()
+			if deadline < end_time:
+				deadline_met = False 
+			else: deadline_met = True
+	else:
+		end_time=None
+		deadline_met = None
 
 	if st.button("Add Task"):
-		add_data(task,task_status,task_due_date)
-		st.success("Added Task \"{}\" ✅".format(task))
+		add_row(title, tag, deadline, deadline_date, about, task_status, time_estimation, start_time, end_time, deadline_met)
+		st.success("Added Task \"{}\" ✅".format(title))
 		st.balloons()
 
-elif choice == "Update Task 👨‍💻":
+elif choice == "Update Task 🖊️":
 	st.subheader("Edit Items")
 	with st.expander("Current Data"):
 		result = view_all_data()
@@ -71,10 +177,10 @@ elif choice == "Update Task 👨‍💻":
 			new_task = st.text_area("Task To Do",task)
 
 		with col2:
-			new_task_status = st.selectbox(task_status,["To Do","Doing","Done"])
+			new_task_status = st.selectbox(task_status,["To-Do","In-Progress","Done"])
 			new_task_due_date = st.date_input(task_due_date)
 
-		if st.button("Update Task 👨‍💻"):
+		if st.button("Update Task 🖊️"):
 			edit_task_data(new_task,new_task_status,new_task_due_date,task,task_status,task_due_date)
 			st.success("Updated Task \"{}\" ✅".format(task,new_task))
 
@@ -104,22 +210,59 @@ elif choice == "Delete Task ❌":
 		clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
 		st.dataframe(clean_df.style.applymap(color_df,subset=['Status']))
 
-else:
-	with st.expander("View All 📝"):
-		result = view_all_data()
-		# st.write(result)
-		clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
-		st.dataframe(clean_df.style.applymap(color_df,subset=['Status']))
+elif choice == "View Tasks' Status 👨‍💻":
+	result = view_all_data()
+	# st.write(result)
+	result_df = pd.DataFrame(result,columns=['guid', 'title', 'tag', 'deadline', 'deadline_date', 'about', 'task_status', 'time_estimation', 'start_time', 'end_time', 'deadline_met'])
+	# clean_df= result_df[['title', 'task_status', 'deadline']] 
+	# clean_df['deadline'] = pd.to_datetime(clean_df['deadline'])
+	# clean_df['deadline']= clean_df['deadline'].dt.date
+	clean_df= result_df[['title', 'task_status', 'deadline_date']] 
 
-	with st.expander("Task Status 📝"):
-		task_df = clean_df['Status'].value_counts().to_frame()
-		task_df = task_df.reset_index()
+	task_df = clean_df['task_status'].value_counts().to_frame()
+	task_df = task_df.reset_index()
+
+	col1,col2 = st.columns(2)
+	with col1:
 		st.dataframe(task_df)
-
-		p1 = px.pie(task_df,names='index',values='Status', color='index',
-			color_discrete_map={'ToDo':'red',
-                                 'Done':'green',
-                                 'Doing':'orange'})
+	with col2:
+		p1 = px.pie(task_df,names='task_status',values='count', color='task_status', color_discrete_map={'To-Do':'red', 'Done':'green', 'In-Progress':'orange'})
 		st.plotly_chart(p1,use_container_width=True)
 
-st.markdown("<br><hr><center>Made with ❤️ by <strong>Bella & Rita</strong></center><hr>", unsafe_allow_html=True)
+	with st.expander("View All 📝"):
+		# result = view_all_data()
+		# # st.write(result)
+		# result_df = pd.DataFrame(result,columns=['guid', 'title', 'tag', 'deadline', 'about', 'task_status', 'time_estimation', 'start_time', 'end_time', 'deadline_met'])
+		# clean_df= result_df[['title', 'task_status', 'deadline']]
+		# clean_df['deadline'] = pd.to_datetime(clean_df['deadline'])
+		# clean_df['deadline']= clean_df['deadline'].dt.date
+		clean_df1= result_df[['title', 'tag', 'task_status', 'deadline_date']]
+		st.dataframe(clean_df1.style.applymap(color_df,subset=['task_status']))
+
+	# with st.expander("Task Status 📝"):
+	# 	task_df = clean_df['task_status'].value_counts().to_frame()
+	# 	task_df = task_df.reset_index()
+	# 	st.dataframe(task_df)
+	# 	p1 = px.pie(task_df,names='task_status',values='count', color='task_status', color_discrete_map={'To-Do':'red', 'Done':'green', 'In-Progress':'orange'})
+	# 	st.plotly_chart(p1,use_container_width=True)
+
+footer1_html = """
+<br>
+<hr>
+<div style="
+    position: relative;
+    inset: 0px auto auto 0px;
+    margin-top: 20px;
+	margin-left: -100px;
+    padding: 10px;
+    color: #333;
+    text-align: center;
+	width: 120%
+">
+    Made with ❤️ by <strong>Bella & Rita</strong>
+</div>
+"""
+# st.markdown(footer1_html, unsafe_allow_html=True)
+st.markdown("<br><hr><center>Made with ❤️ by <strong>Bella & Rita</strong></center>", unsafe_allow_html=True)
+
+# footer()
--- db_funcs.py
+++ db_funcs.py
@@ -1,43 +1,44 @@
 import sqlite3
-conn = sqlite3.connect('data.db',check_same_thread=False)
+conn = sqlite3.connect('tasks.db',check_same_thread=False)
 c = conn.cursor()
 
-
 def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS taskstable(task TEXT,task_status TEXT,task_due_date DATE)')
+    c.execute('CREATE TABLE IF NOT EXISTS tasks ( title TEXT, tag TEXT, deadline DATETIME, about TEXT, task_status TEXT, time_estimation TEXT, start_time DATETIME, end_time DATETIME, deadline_met BOOL )')
 
-
-def add_data(task,task_status,task_due_date):
-	c.execute('INSERT INTO taskstable(task,task_status,task_due_date) VALUES (?,?,?)',(task,task_status,task_due_date))
-	conn.commit()
-
-
+def add_row(title, tag, deadline, deadline_date, about, task_status, time_estimation, start_time, end_time, deadline_met):
+    c.execute('INSERT INTO tasks(title, tag, deadline, deadline_date, about, task_status, time_estimation, start_time, end_time, deadline_met) VALUES (?,?,?,?,?,?,?,?,?,?)',(title, tag, deadline, deadline_date, about, task_status, time_estimation, start_time, end_time, deadline_met))
+    conn.commit()
+    
 def view_all_data():
-	c.execute('SELECT * FROM taskstable')
-	data = c.fetchall()
-	return data
+    c.execute('SELECT * FROM tasks')
+    data = c.fetchall()
+    return data
 
 def view_all_task_names():
-	c.execute('SELECT DISTINCT task FROM taskstable')
-	data = c.fetchall()
-	return data
-
+    c.execute('SELECT DISTINCT title FROM tasks')
+    data = c.fetchall()
+    return data
+              
 def get_task(task):
-	c.execute('SELECT * FROM taskstable WHERE task="{}"'.format(task))
-	data = c.fetchall()
-	return data
+    c.execute('SELECT * FROM tasks WHERE title="{}"'.format(task))
+    data = c.fetchall()
+    return data
 
+def get_today_tasks(deadline_date):
+    c.execute('SELECT * FROM tasks WHERE deadline_date="{}"'.format(deadline_date))
+    data = c.fetchall()
+    return data
+              
 def get_task_by_status(task_status):
-	c.execute('SELECT * FROM taskstable WHERE task_status="{}"'.format(task_status))
-	data = c.fetchall()
-
-
-def edit_task_data(new_task,new_task_status,new_task_date,task,task_status,task_due_date):
-	c.execute("UPDATE taskstable SET task =?,task_status=?,task_due_date=? WHERE task=? and task_status=? and task_due_date=? ",(new_task,new_task_status,new_task_date,task,task_status,task_due_date))
-	conn.commit()
-	data = c.fetchall()
-	return data
-
+    c.execute('SELECT * FROM tasks WHERE task_status="{}"'.format(task_status))
+    data = c.fetchall()
+              
+# def edit_task_data(new_task,new_task_status,new_task_date,task,task_status,task_due_date):
+#     c.execute("UPDATE taskstable SET task =?,task_status=?,task_due_date=? WHERE task=? and task_status=? and task_due_date=? ",(new_task,new_task_status,new_task_date,task,task_status,task_due_date))
+#     conn.commit()
+#     data = c.fetchall()
+#     return data
+              
 def delete_data(task):
-	c.execute('DELETE FROM taskstable WHERE task="{}"'.format(task))
-	conn.commit()
+    c.execute('DELETE FROM tasks WHERE title="{}"'.format(task))
+    conn.commit()
